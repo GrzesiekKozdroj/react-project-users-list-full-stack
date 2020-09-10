d38880b185895884311291559a8bf123cffc5144
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2020-09-10 20:07:57 +0100

    styled and tested.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom'
 import axios from 'axios'
 import store from './store/store.js'
 import addUsers from './actions/users.js'
-
+import Header from './components/Header.js'
+import UserList from './components/UserList.js'
+import { Provider } from 'react-redux'
+import './css/style.css'
 
 class App extends React.Component {
     componentDidMount(){
@@ -15,9 +18,18 @@ class App extends React.Component {
     }
     render() {
         return (
-            <h1>Welcome to REACT!!</h1>
+            <div>
+                <Header />
+                <UserList />
+            </div>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store = { store }>
+        <App />
+    </Provider>
+    , 
+    document.getElementById('root')
+)
